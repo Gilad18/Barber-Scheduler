913fceab9e56a7b6bbe63219aced47a1199f4db9
Gilad <gilad18587@gmail.com>
2021-05-30 15:58:49 +0300

    deploy

--- barber/src/components/utility.js
+++ barber/src/components/utility.js
@@ -1,4 +1,6 @@
-export const DATABASE = 'http://localhost:3900/mybarber/api'
+// export const DATABASE = 'http://localhost:3900/mybarber/api'
+export const DATABASE = 'https://mybarber-schedule.herokuapp.com/mybarber/api'
+
 
 export const avaiabilty = ['10:00', '10:30', '11:00', '11:30', '12:00', '12:30', '13:00', '13:30'
     , '14:00', '14:30', '16:00', '16:30', '17:00', '17:30',
